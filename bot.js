1e1ec86c0d490c2d7eb2dafdac4a25e3146787ee
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-05-26 22:38:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("By!♤O.H♤|| ╲⎝مجهول☠☯☠NaSto⎠╱ ||#0831 ")
+                    .setFooter("By ➣҈в̿̿s✷ ╫₫aṜฬïห҉҈ް✷#1145 ")
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
